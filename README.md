4c8d97096a3844864eda3d3949a41df24ad8111f
jc3213 <jc3213@users.noreply.github.com>
2024-05-30 16:18:48 +0800

    Update metalink.js

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# metalink.js
-A Javascript library to create `.metalink` or `.meta4` files from JSON
+## Usage
+
+### Download
+[Latest](https://jc3213.github.io/metalink.js/metalink.js)
+
+### HTML
+```HTML
+<script src="https://jc3213.github.io/metalink.js/metalink.js"></script>
+```
+
+### TamperMonkey
+```javascript
+// @require https://jc3213.github.io/metalink.js/metalink.js
+```
+
+## Syntax
+```javascript
+let file1 = {url: {url: 'https://jc3213.github.io/metalink.js/metalink.js', location: 'en'}, version: '0.1.0'};
+let file2 = {url: 'https://github.com/jc3213/metalink.js/archive/refs/heads/main.zip', name: 'metalink.js_by_@jc3213.zip'}
+let metalink = new Metalink(file1, file2);
+```
+
+### Object
+| Key       | Description | Details |
+|-----------|-------------|:------------:|
+| `url`    | file urls   | **required** <br> `String` or `Object` or `Array` |
+| `name`    | file name   | *optional* |
+| `size`    | file size   | *optional* |
+| `version` | file version | *optional* |
+| `language`| language    | *optional* |
+| `hash`  | file hashes | *optional* <br> `Object` or `Array` |
+| `metaurl`| the metaurls | *optional* <br> `Object` or `Array` |
+
+## Method
+- [save](#save)
+
+### save
+```javascript
+metalink.save(filename);
+```
+- filename
+    - `String`
+    - The file name you'd like to save
+    - Default `metalink_yymmdd_hhmmss`
---
+++ metalink.js
@@ -0,0 +1,58 @@
+class Metalink {
+    constructor (...args) {
+        let encoder = new TextEncoder();
+        let metalink = ['<?xml version="1.0" encoding="UTF-8"?>\n<metalink xmlns="urn:ietf:params:xml:ns:metalink">', ...args.map(this.meta4), '</metalink>'];
+        this.text = metalink.join('\n');
+        this.lines = this.text.split(/\n\s*/);
+        this.arrayBuffer = encoder.encode(this.text);
+        this.dataURL = 'data:text/plain;base64,' + btoa(unescape(encodeURIComponent(this.text)));
+        this.blob = new Blob([this.text], {type: 'application/metalink+xml; charset=utf-8'});
+    }
+    meta4 (arg) {
+        let file = '    ';
+        let {name, size, version, language, hash = [], url, metaurl = []} = arg;
+        file += name ? '<file name="' + name + '">' : '<file>';
+        if (size) {
+            file += '\n        <size>' + size + '</size>';
+        }
+        if (version) {
+            file += '\n        <version>' + version + '</version>';
+        }
+        if (language) {
+            file += '\n        <language>' + language + '</language>';
+        }
+        let hashes = Array.isArray(hash) ? hash : [hash];
+        hashes.forEach(({type, hash}) => file += '\n        <hash type="' + type + '">' + hash + '</hash>');
+        let urls = Array.isArray(url) ? url : [url];
+        urls.forEach((arg) => {
+            if (typeof arg === 'object') {
+                let {location, url} = arg;
+                file += location ?  '\n        <location="' + location + '">' + url + '</url>' : '\n        <url>' + url + '</url>';
+            } else if (typeof arg === 'string') {
+                file += '\n        <url>' + arg + '</url>';
+            }
+        });
+        let metaurls = Array.isArray(metaurl) ? metaurl : [metaurl];
+        metaurls.forEach(({type, url}) => file += '\n        <metaurl metatype="' + type + '">' + url + '</metaurl>');
+        return file + '\n    </file>';
+    }
+    save (filename) {
+        if (!filename) {
+            let date = new Date();
+            let year = date.getFullYear();
+            let month = ('0' + (date.getMonth() + 1)).slice(-2);
+            let day = ('0' + date.getDate()).slice(-2);
+            let hours = ('0' + date.getHours()).slice(-2);
+            let minutes = ('0' + date.getMinutes()).slice(-2);
+            let seconds = ('0' + date.getSeconds()).slice(-2);
+            filename = 'metalink_' + year + month + day + '_' + hours + minutes + seconds;
+        }
+        let url = URL.createObjectURL(this.blob);
+        let a = document.createElement('a');
+        a.href = url;
+        a.download = filename + '.metalink';
+        a.click();
+        a.remove();
+        URL.revokeObjectURL(url);
+    }
+}
